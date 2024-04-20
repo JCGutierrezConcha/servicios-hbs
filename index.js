8aeddb411c458b1cfe3fab7847cc84147ea2cef4
JCGutierrezConcha <jc.gutierrez.concha@gmail.com>
2024-04-20 15:40:06 -0400

    Agrega carpeta data con array de servicios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { engine } from 'express-handlebars';
 import path from 'path'
+import { services } from './data/services.data.js'
 
 const app = express()
 
@@ -15,50 +16,6 @@ app.use('/assets/js', express.static(path.join(__dirname, '/node_modules/bootstr
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', __dirname + '/views');
-
-
-const services = [
-    {
-        name: "Web Development",
-        description: "We create web applications with the latest technologies.",
-        url: "/services/web-development",
-    },
-    {
-        name: "Mobile Development",
-        description: "We create mobile applications with the latest technologies.",
-        url: "/services/mobile-development",
-    },
-    {
-        name: "DevOps",
-        description: "We create CI/CD pipelines with the latest technologies.",
-        url: "/services/devops",
-    },
-    {
-        name: "QA",
-        description: "We create test cases with the latest technologies.",
-        url: "/services/qa",
-    },
-    {
-        name: "UX/UI",
-        description: "We create designs with the latest technologies.",
-        url: "/services/ux-ui",
-    },
-    {
-        name: "Training",
-        description: "We create training with the latest technologies.",
-        url: "/services/training",
-    },
-    {
-        name: "Consulting",
-        description: "We create consulting with the latest technologies.",
-        url: "/services/consulting",
-    },
-    {
-        name: "Outsourcing",
-        description: "We create outsourcing with the latest technologies.",
-        url: "/services/outsourcing",
-    },
-]
 
 app.get('/services', (req, res) => {
     res.render('services', { services: services })
